e10e6087ff6063cae921f40a5ee86fbe92a30ac5
Eugen Guriev <gurievcreative@gmail.com>
2022-10-02 21:14:28 +0300

    chore: add license

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-const yargs = require("yargs/yargs");
-const { hideBin } = require("yargs/helpers");
-const { execSync } = require("child_process");
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { execSync } = require('child_process');
 
 yargs(hideBin(process.argv))
   .command(
-    "generate [swaggerPath] [tsPath]",
-    "generate typescript schemas",
+    'generate [swaggerPath] [tsPath]',
+    'generate typescript schemas',
     (yargs) =>
       yargs
-        .positional("swaggerPath", {
-          describe: "path to swagger json file",
-          default: "",
+        .positional('swaggerPath', {
+          describe: 'path to swagger json file',
+          default: '',
         })
-        .positional("tsPath", {
-          describe: "target ts path",
-          default: "",
+        .positional('tsPath', {
+          describe: 'target ts path',
+          default: '',
         }),
     (argv) => {
       execSync(`openapi-typescript ${argv.swaggerPath} -o ${argv.tsPath}`);
